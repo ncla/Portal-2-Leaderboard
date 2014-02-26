a7fbc1c387d44d5d2229b2a2797da124f0add99a
iamncla <blindmind6@gmail.com>
2014-02-26 20:35:46 +0200

    Removed Least Portal updating

--- fetchnew.php
+++ fetchnew.php
@@ -3,14 +3,14 @@ include('model.php');
 include('db_conf.php');
 new Leaderboard();
 
-$contents = file_get_contents("lp.txt");
-
-$atm = time();
-$difference = $atm - $contents;
-
-if(round($difference) > 3600) {
-	new LeastPortals;
-	$currenttime = time();
-	file_put_contents("lp.txt", $currenttime);
-}
+//$contents = file_get_contents("lp.txt");
+//
+//$atm = time();
+//$difference = $atm - $contents;
+//
+//if(round($difference) > 3600) {
+//	new LeastPortals;
+//	$currenttime = time();
+//	file_put_contents("lp.txt", $currenttime);
+//}
 ?>
