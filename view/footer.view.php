24344917907957e6c88529b6e4eb492350dd95c4
iamncla <blindmind6@gmail.com>
2014-03-17 14:33:29 +0200

    Footer text positioning derpiness fix

--- view/footer.view.php
+++ view/footer.view.php
@@ -1,4 +1,7 @@
 <div id="footer">
-    <div class="footerleft">Developed, designed and hosted by <a href="https://twitter.com/iamncla" target="_blank">Nuclear</a><br/><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VESN9DWJ45DCS">Support further development and hosting expenses by donating</a></div>
+    <div class="footerleft">
+        <div>Developed, designed and hosted by <a href="https://twitter.com/iamncla" target="_blank">Nuclear</a></div>
+        <div><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VESN9DWJ45DCS">Support further development and hosting expenses by donating</a></div>
+    </div>
     <div class="footeright">This page was generated in <?php echo microtime(true) - $this->exec_time; ?> seconds</div>
 </div>
