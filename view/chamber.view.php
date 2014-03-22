cf046e22d8566a14b8d25dd580d6d6528d86387d
iamncla <blindmind6@gmail.com>
2014-03-22 12:42:14 +0200

    Force stylesheet update for clients with cache control

--- view/chamber.view.php
+++ view/chamber.view.php
@@ -7,7 +7,7 @@
 <title>Leaderboards</title>
 
 <link rel="stylesheet" type="text/css" href="/style.css"></link>
-<link rel="stylesheet" type="text/css" href="/style_chamber.css"></link>
+<link rel="stylesheet" type="text/css" href="/stylechamber.css"></link>
 
 <script type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>
 <!--<script type="text/javascript" src="/js/jquery.domchanged.min.js"></script>-->
